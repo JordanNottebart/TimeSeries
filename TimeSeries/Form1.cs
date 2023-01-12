ff02549636249bfd3ea5d21ccda3c2a14c714656
Robin D'hallewin <robin.dhallewin@hotmail.com>
2023-01-12 15:06:13 +0100

    Commentaar

--- TimeSeries/Form1.cs
+++ TimeSeries/Form1.cs
@@ -25,12 +25,12 @@ namespace TimeSeries
 
         private void openFile_Click(object sender, EventArgs e)
         {
-            OpenFileDialog explorer = new OpenFileDialog();
-            explorer.Title = "Choose a file";
-            explorer.Filter = "Text Files (*.txt)|*.txt;|CSV Files (*.csv)|*.csv;|Excel Worksheets (*.xls;*.xlsx)|*.xls;*.xlsx";
-            if (explorer.ShowDialog() == DialogResult.OK)
+            OpenFileDialog explorer = new OpenFileDialog();//Maakt een verkenner window aan
+            explorer.Title = "Choose a file";//Maakt titel van het window
+            explorer.Filter = "Text Files (*.txt)|*.txt;|CSV Files (*.csv)|*.csv;|Excel Worksheets (*.xls;*.xlsx)|*.xls;*.xlsx";//
+            if (explorer.ShowDialog() == DialogResult.OK)//Als er iets is geselecteerd
             {
-                pathofthefile.Text = explorer.FileName;
+                pathofthefile.Text = explorer.FileName;//Zet de path naar de label
             }
 
         }
