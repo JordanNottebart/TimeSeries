442460b7e5892a8e3ccd8e290cf9e49ab7144a67
miichielduy <michielduyvejonck001@gmail.com>
2023-01-16 15:07:51 +0100

    start

---
+++ TimeSeries/Test/TS11_Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeSeries
+{
+    public class TS11_Test : TS_ITest
+    {
+
+    }
+}
---
+++ TimeSeries/Test/TS_ITest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeSeries
+{
+    internal interface TS_ITest
+    {
+        public string PerformTest(BucketList timesSeries);
+    }
+}
