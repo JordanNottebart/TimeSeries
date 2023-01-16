00fda61ce43f832df785758cb02893a9b838c372
Robin D'hallewin <robin.dhallewin@hotmail.com>
2023-01-16 16:09:24 +0100

    Interface bucket testing

---
+++ TimeSeries/Test/TS_ITestBucket.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeSeries.Test
+{
+    interface TS_ITestBucket//Interface om buckets te testen
+    {
+        public bool PerformTestBucket(Bucket bucket);
+    }
+}
