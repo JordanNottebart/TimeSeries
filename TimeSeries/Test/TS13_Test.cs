8ca707137d157f1b2336339bde00ff99af4257de
JordanNottebart <jordan.nottebart@gmail.com>
2023-01-19 14:14:54 +0100

    Test 13 and test 14

---
+++ TimeSeries/Test/TS13_Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeSeries
+{
+    public class TS13_Test : TS_ITestFileFormat
+    {
+        public bool PerformTest(string[] titleRow)
+        {
+            // Chech if the title of the first colmun in the title row is "start" (case insensitive)
+            if (titleRow[0].ToLower() == "start")
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ TimeSeries/Test/TS14_Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeSeries
+{
+    public class TS14_Test : TS_ITestFileFormat
+    {
+        public bool PerformTest(string[] titleRow)
+        {
+            // Chech if the title of the first colmun in the title row is "end" (case insensitive)
+            if (titleRow[0].ToLower() == "end")
+            {
+                return true;
+            }
+            
+            return false;
+        }
+    }
+}
