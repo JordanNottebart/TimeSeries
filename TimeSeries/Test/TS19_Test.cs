dcfb7a0333b274ba969e29482f3802c3e657531a
Jordan Nottebart <jordan.nottebart@gmail.com>
2023-01-23 15:34:05 +0100

    Added Test 19

--- TimeSeries/Test/TS19_Test.cs
+++ TimeSeries/Test/TS19_Test.cs
@@ -8,9 +8,11 @@ namespace TimeSeries.Test
     {
         public bool PerformTestBucket(Bucket bucket)
         {
+            // Variables
             DateTime dateLowerLimit = new DateTime(0001, 01, 01, 0, 0, 0);
             DateTime dateUpperLimit = new DateTime(9999, 12, 31, 23, 59, 59);
             
+            // If the startDate or the endDate is not in between the limits
             if (bucket.startDate < dateLowerLimit || bucket.startDate > dateUpperLimit || bucket.endDate < dateLowerLimit || bucket.endDate > dateUpperLimit)
             {
                 return false;
