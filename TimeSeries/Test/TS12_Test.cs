e61be71b5c6c3bafef5557655c5e6079e0fd8cc8
miichielduy <michielduyvejonck001@gmail.com>
2023-01-23 15:09:09 +0100

    Hopla

--- TimeSeries/Test/TS12_Test.cs
+++ TimeSeries/Test/TS12_Test.cs
@@ -11,7 +11,14 @@ namespace TimeSeries.Test
         public bool PerformTest(string[] splitstring)
         {
             ErrorMessage = "Er bestaat geen titelrij";
-            return true;
+            TS_ITestFileFormat x = new TS13_Test();
+            TS_ITestFileFormat y = new TS14_Test();
+            if (x.PerformTest(splitstring) && y.PerformTest(splitstring))
+            {
+                return true;
+            }
+            return false;
+
         }
     }
 }
