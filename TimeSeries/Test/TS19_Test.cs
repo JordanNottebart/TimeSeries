65af9a8fe11a23c001b5ed29dc096a5433dcedb4
Jordan Nottebart <jordan.nottebart@gmail.com>
2023-01-23 15:26:36 +0100

    M

---
+++ TimeSeries/Test/TS19_Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeSeries.Test
+{
+    class TS19_Test : TS_ITestBucket
+    {
+        public bool PerformTestBucket(Bucket bucket)
+        {
+            DateTime dateLowerLimit = new DateTime(0001, 01, 01, 0, 0, 0);
+            DateTime dateUpperLimit = new DateTime(9999, 12, 31, 23, 59, 59);
+            
+            if (bucket.startDate < dateLowerLimit || bucket.startDate > dateUpperLimit || bucket.endDate < dateLowerLimit || bucket.endDate > dateUpperLimit)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
