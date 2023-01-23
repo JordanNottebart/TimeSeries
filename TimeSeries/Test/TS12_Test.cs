aed121626a5e71bcec69e9bb92f7547b0ce21346
miichielduy <michielduyvejonck001@gmail.com>
2023-01-23 14:33:04 +0100

    Test 12 aangemaakt

---
+++ TimeSeries/Test/TS12_Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeSeries.Test
+{
+    public class TS12_Test : TS_ITestFileFormat
+    {
+        public string ErrorMessage { get ; set ; }
+
+        public bool PerformTest(string[] splitstring)
+        {
+            ErrorMessage = "Er bestaat geen titelrij";
+            return true;
+        }
+    }
+}
