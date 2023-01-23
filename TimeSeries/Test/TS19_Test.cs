31013516c8bd0b07178f8fb5a5a6bc7f131e9b79
Jordan Nottebart <jordan.nottebart@gmail.com>
2023-01-23 16:11:55 +0100

    Added an error message in the 19th test

--- TimeSeries/Test/TS19_Test.cs
+++ TimeSeries/Test/TS19_Test.cs
@@ -11,7 +11,8 @@ namespace TimeSeries.Test
             // Variables
             DateTime dateLowerLimit = new DateTime(0001, 01, 01, 0, 0, 0);
             DateTime dateUpperLimit = new DateTime(9999, 12, 31, 23, 59, 59);
-            
+
+            ErrorMessage = " De datum bestaat niet.";
             // If the startDate or the endDate is not in between the limits
             if (bucket.startDate < dateLowerLimit || bucket.startDate > dateUpperLimit || bucket.endDate < dateLowerLimit || bucket.endDate > dateUpperLimit)
             {
@@ -22,5 +23,8 @@ namespace TimeSeries.Test
                 return true;
             }
         }
+
+        public string ErrorMessage { get; set; }
     }
 }
+
